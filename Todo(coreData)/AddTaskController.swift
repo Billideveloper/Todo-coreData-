4faa6a1498e38da98bbbcb659517393d0e7f47cb
Billidevelopers <billidevlopers@gmail.com>
2020-09-24 21:30:30 +0530

    load and add task

--- Todo(coreData)/AddTaskController.swift
+++ Todo(coreData)/AddTaskController.swift
@@ -10,6 +10,8 @@ import UIKit
 class AddTaskController: UIViewController {
     
     
+    @IBOutlet weak var addtaskImage: UIImageView!
+    
     @IBOutlet weak var newtask: UITextField!
     
     @IBOutlet weak var addsmileybtn: UISwitch!
@@ -22,6 +24,15 @@ class AddTaskController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - imageview animation
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        UIView.animate(withDuration: 1.0) {
+            self.addtaskImage.transform = self.addtaskImage.transform.rotated(by: CGFloat(Double.pi))
+        }
+        
+    }
 
   
     @IBAction func addSmileybtn(_ sender: Any) {
@@ -36,12 +47,34 @@ class AddTaskController: UIViewController {
     @IBAction func addTaskbtn(_ sender: Any) {
     //add task here
         
+        let mydelegates = UIApplication.shared.delegate as! AppDelegate
         
+        let context = mydelegates.persistentContainer.viewContext
+        
+        let mytask = Task(context: context)
+        
+      
+        if let taskname = newtask.text{
+            mytask.name = taskname
+        }
+        
+        mytask.smiley = addsmileybtn.isOn
+        
+        mydelegates.saveContext()
+        
+        self.dismiss(animated: true, completion: nil)
         
         
     }
     
     
     
+    @IBAction func cancel(_ sender: Any) {
+        
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        
+    }
     
 }
--- Todo(coreData)/MainVc.swift
+++ Todo(coreData)/MainVc.swift
@@ -6,12 +6,68 @@
 //
 
 import UIKit
+import CoreData
 
-class MainVc: UIViewController {
 
+
+class MainVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mytask : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        fetchdata()
+        
+        self.tableView.reloadData()
+    
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mytask.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let taskname = mytask[indexPath.row]
+        
+        cell.textLabel?.text = "\(String(describing: taskname.name))"
+        
+        return cell
+    }
+    
+    func fetchdata(){
+        
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = delegate.persistentContainer.viewContext
+        
+        do{
+            mytask = try context.fetch(Task.fetchRequest())
+            
+            tableView.reloadData()
+            
+        } catch{
+            
+            debugPrint(error)
+            
+        }
+        
     }
 
 
