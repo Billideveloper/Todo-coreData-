a4e249342170fb7a638b53a6d1a910ef4096029f
Billidevelopers <billidevlopers@gmail.com>
2020-09-24 17:31:26 +0530

    added UI

---
+++ Todo(coreData)/AddTaskController.swift
@@ -0,0 +1,47 @@
+//
+//  AddTaskController.swift
+//  Todo(coreData)
+//
+//  Created by Ravi Thakur on 24/09/20.
+//
+
+import UIKit
+
+class AddTaskController: UIViewController {
+    
+    
+    @IBOutlet weak var newtask: UITextField!
+    
+    @IBOutlet weak var addsmileybtn: UISwitch!
+    
+    //MARK: - viewdidLoad
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+    @IBAction func addSmileybtn(_ sender: Any) {
+        
+        
+        
+    }
+    
+    //MARK: - addtaskfunc
+    
+    
+    @IBAction func addTaskbtn(_ sender: Any) {
+    //add task here
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
